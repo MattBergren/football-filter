2f709bb16090cf0d5c1de30b75a407568599d1a9
Matthew Bergren <matthewbergren@mbp.lan>
2022-05-12 10:21:07 -0500

    removing service worker

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,29 @@
 import React, { Component } from "react";
-import './App.css';
-import DropdownTeams from './components/DropdownTeams';
-import RadioPositions from './components/RadioPositions';
-import TogglePro from './components/TogglePro';
-import CardList from './components/CardList';
+import "./App.css";
+import DropdownTeams from "./components/DropdownTeams";
+import RadioPositions from "./components/RadioPositions";
+import TogglePro from "./components/TogglePro";
+import CardList from "./components/CardList";
 
 class App extends Component {
-  
-  // initialize our state 
+  // initialize our state
   state = {
-    team: 'all',
-    position: 'all',
-    proBowl: false
+    team: "all",
+    position: "all",
+    proBowl: false,
   };
 
   onTeamSelect = (team) => {
     this.setState({ team });
-  }
+  };
 
   onPositionSelect = (position) => {
     this.setState({ position });
-  }
+  };
 
   onProToggle = (proBowl) => {
     this.setState({ proBowl });
-  }
+  };
 
   render() {
     return (
@@ -36,26 +35,21 @@ class App extends Component {
               <div className="ui sub header">Team</div>
               <DropdownTeams onTeamSelect={this.onTeamSelect} />
               <div className="ui sub header">Position</div>
-              <RadioPositions onPositionSelect={this.onPositionSelect}/>
+              <RadioPositions onPositionSelect={this.onPositionSelect} />
               <TogglePro onProToggle={this.onProToggle} />
-            </aside> 
+            </aside>
           </div>
           <div className="eleven wide column">
-            <CardList 
-              team={this.state.team} 
-              position={this.state.position} 
-              proBowl={this.state.proBowl} 
+            <CardList
+              team={this.state.team}
+              position={this.state.position}
+              proBowl={this.state.proBowl}
             />
           </div>
         </div>
       </div>
-
     );
-
   }
-
-
-
 }
 
 export default App;
